20de0ddb4fa0cf811efb2f65147f05460a0a9944
srbelk <srbelk@gmail.com>
2026-05-15 16:08:41 -0400

    updated main to work with paths to multiple books

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sorted_dict_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text():
-       with open("books/frankenstein.txt", encoding="utf-8", newline="") as book:
+       with open(sys.argv[1]) as book:
             return book.read()
 
 def main():
     book_string = get_book_text()
 
-    print(f"Found {count_words(book_string)} total words")
-
     characters = count_characters(book_string)
 
-    print(characters)
+    sorted_characters = sorted_dict_list(characters)
+
+    print("============ BOOKBOT ============")
+
+    print(f"Analyzing book found at {sys.argv[1]}...")
+
+    print("----------- Word Count ----------")
+
+    print(f"Found {count_words(book_string)} total words")
+
+    print("--------- Character Count -------")
+    for item in sorted_characters:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+
+    print("============= END ===============")
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from typing import TypedDict
+
 def count_words(text):
     words = text.split()
 
@@ -21,6 +23,27 @@ def count_characters(text):
               char_dict[char] += 1
 
     return char_dict
+
+class CharacterCount(TypedDict):
+     char: str
+     num: int
+
+def sorted_dict_list(characters):
+    list_of_dict = []
+
+    for key in characters:
+        list_of_dict.append({"char": key, "num": characters[key] })
+
+    list_of_dict.sort(reverse = True, key = help_sorted_dict_list)
+
+    return list_of_dict
+
+def help_sorted_dict_list(CharacterCount):
+    return CharacterCount["num"]
+
+
+
+
 
 
 
